38c6b7fe895e06f4fca49f5f31eeeda2b6f324de
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-10-31 11:28:59 +0000

    Improved output
    We display the result of all mapping applications (red->green).
    
    A summary list of outstanding mappings is printed, with line numbers in
    the mappings file to aid finding the right line to change.

--- clean_encoding.rb
+++ clean_encoding.rb
@@ -79,17 +79,22 @@ end
 class Mappings
   attr_reader :mappings
 
+  class Mapping < Struct.new(:line_number, :bad_sequence, :replacement)
+  end
+
   def initialize
-    @mappings = Hash.new
+    @mappings = []
     load
   end
 
   def load
+    line_number = 1
     File.open(filename, 'r:binary').read.chomp.split("\n").map do |line|
       bad_sequence, replacement = parse_line(line)
-      @mappings[bad_sequence] = replacement
+      @mappings << Mapping.new(line_number, bad_sequence, replacement)
+      line_number += 1
     end
-  rescue Errno::ENOENT => e
+  rescue Errno::ENOENT
     []
   end
 
@@ -101,13 +106,13 @@ class Mappings
 
   def save(file = nil)
     File.open(file || filename, 'w:binary') do |f|
-      @mappings.each do |bad_sequence, replacement|
-        bytestring = '\x' + bad_sequence.each_byte.map do |byte|
+      @mappings.each do |mapping|
+        bytestring = '\x' + mapping.bad_sequence.each_byte.map do |byte|
           byte.to_s(16).upcase
         end.join('\x')
         f.write(bytestring)
         f.write(":")
-        f.write(replacement || "TODO")
+        f.write(mapping.replacement || "TODO")
         f.write("\n")
       end
     end
@@ -117,49 +122,100 @@ class Mappings
     "mappings.txt"
   end
 
-  def include?(bad)
-    @mappings.has_key?(bad.to_s)
+  def find_mapping(bad_sequence)
+    @mappings.find { |mapping| mapping.bad_sequence == bad_sequence.to_s }
   end
 
-  def done?(bad)
-    ! @mappings[bad.to_s].nil?
+  def include?(bad_sequence)
+    ! find_mapping(bad_sequence).nil?
   end
 
-  def add(bad)
-    @mappings[bad.to_s] = nil unless @mappings.has_key?(bad.to_s)
+  def done?(bad_sequence)
+    mapping = find_mapping(bad_sequence)
+    mapping && mapping.replacement
+  end
+
+  def add(bad_sequence)
+    @mappings << Mapping.new(@mappings.size + 1, bad_sequence.to_s, nil) unless include?(bad_sequence.to_s)
+    find_mapping(bad_sequence).line_number
   end
 
   def fix(data)
     new_data = data.dup
-    in_order = @mappings.sort_by {|k,v| k}.reverse
-    first_nil_idx = in_order.find_index {|bad_sequence, replacement| replacement.nil?} || in_order.size
-    in_order[0...first_nil_idx].each do |bad_sequence, replacement|
-      new_data.gsub!(bad_sequence, replacement)
+    in_order = @mappings.sort_by {|mapping| mapping.bad_sequence.size}.reverse
+    first_nil_idx = in_order.find_index {|mapping| mapping.replacement.nil?} || in_order.size
+    in_order[0...first_nil_idx].each do |mapping|
+      new_data = apply_replacement(new_data, mapping.bad_sequence, mapping.replacement, mapping.line_number)
     end
     new_data
   end
 
+  def apply_replacement(data, sequence, replacement, line_number)
+    pos = 0
+    shown = false
+    found = 0
+
+    puts "\n#{line_number}: => #{replacement} "
+    while from = data.index(sequence, pos)
+      to = from + sequence.size - 1
+      new_data = data.dup
+      new_data[from..to] = replacement
+      new_data.freeze
+      new_to = from + replacement.size - 1
+      show(data, from, to, :red) unless shown
+      show(new_data, from, new_to, :green) unless shown
+      data = new_data
+      pos = new_to
+      found +=1
+    end
+    puts "   ... (replaced #{found})"
+    data
+  end
+
+  def show(data, from, to, background_colour, context=30)
+    context_start = [0, from-context].max
+    context_end = [data.size, to+context].min
+    puts(
+      data[context_start...from].gsub("\n", "\\n") +
+      data[from..to].white.colorize(background: background_colour) +
+      data[(to+1)..context_end].gsub("\n", "\\n")
+      )
+  end
+
   def is_replacement_target?(str)
-    @mappings.values.include?(str)
+    @mappings.any? { |mapping| mapping.replacement == str }
   end
 end
 
 
 mappings = Mappings.new
 data = mappings.fix(data)
+
+partitioner = FilePartitioner.new(/[\x80-\xff]+/n)
+parts = partitioner.partition(data)
+puts "\n\nRemaining unmapped bad sequences:"
+number_remaining = 0
+parts.each_slice(2) do |good, bad|
+  next unless bad
+  next if mappings.is_replacement_target?(bad.to_s)
+  next if mappings.done?(bad)
+  number_remaining += 1
+  mapping_number = mappings.add(bad)
+  puts "#{mapping_number}: #{bad.in_context}"
+end
+
+if number_remaining == 0
+  puts "  None."
+end
+
+puts "\n"
 if output_file
   File.open(output_file, 'w:binary') do |f|
     f.write(data)
   end
-end
-
-f = FilePartitioner.new(/[\x80-\xff]+/n)
-parts = f.partition(data)
-parts.each_slice(2) do |good, bad|
-  next unless bad
-  next if mappings.is_replacement_target?(bad.to_s)
-  mappings.add(bad)
-  puts bad.in_context unless mappings.done?(bad)
+  puts "Wrote cleaned file to #{output_file}."
+else
+  puts "No output file requested."
 end
 
 mappings.save
